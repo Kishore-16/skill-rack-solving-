f99d977bf7d6335ec341576650f2277f6531340e
kishore <arvindkishore931@gmail.com>
2026-08-17 23:35:44 +0530

    Add skillrack_49.java

---
+++ skillrack_49.java
@@ -0,0 +1,31 @@
+/*JAVA - VERY EASY- PART002
+ProgramID-1911
+SKILLRACK
+Volume of a cuboid
+The length L, breadth B and height H of a cuboid are passed as input and the program must print the volume of the cuboid as the output.
+Input Format:
+The first line denotes L
+The second line denotes B
+The third line denotes H
+Output Format:
+The first line denotes the volume of the cuboid.
+Example Input/Output:
+Input:
+2
+4
+5
+Output:
+40
+*/
+import java.util.*;
+
+public class skillrack_49 {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextInt()*sc.nextInt()*sc.nextInt());
+		sc.close();
+	}
+
+}

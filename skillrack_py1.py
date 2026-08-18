8868e14079dbc7786415401ebe365716da7bb746
kishore <arvindkishore931@gmail.com>
2026-08-18 23:52:34 +0530

    simple python program

---
+++ skillrack_py1.py
@@ -0,0 +1,20 @@
+"""Python-VERY-EASY-PART001
+ProgramID-1872 SKILLRACK
+Repeat the input number
+Nitish is studying in fifth standard and he wants to write a program which will accept an input number N and then print the number with the message program. Help Nitish by completing the
+Please note that the output is case sensitive and hence Y in You must be in upper case.
+Boundary Conditions:
+0 <= N <= 100
+Example Input/Output 1:
+Input: 50
+Output
+You entered 50
+Example Input/Output 2:
+Input: 4
+Output
+You entered 4
+Max Execution Time Limit: 5000 millisecs"""
+
+#Your code below
+n= input()
+print("You entered",n)
